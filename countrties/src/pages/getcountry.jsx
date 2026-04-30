765c962d0c07c0719d04314fea178eb7c2831ee0
Behruz <behruzyunusboyev68@gmail.com>
2026-04-30 16:31:21 +0500

    filter va chegara dv lar qoshish kerak

--- countrties/src/pages/getcountry.jsx
+++ countrties/src/pages/getcountry.jsx
@@ -5,21 +5,17 @@ function GetCountry() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const randomnumber = Math.floor(Math.random() * 249) + 1;
     const getCountryData = async () => {
         try {
             setLoading(true);
             const res = await fetch("https://restcountries.com/v3.1/all?fields=name,capital,currencies,continents,borders,flags,languages,population,region");
             
             const data = await res.json();
-
-            setCountry({
-                   name: data[randomnumber].name.common,
-                   flag: data[randomnumber].flags.png, 
-                poytaxt: data[randomnumber].capital ? data[randomnumber].capital[0] : "N/A",
-                   qita: data[randomnumber].continents ? data[randomnumber].continents[0] : "N/A",
-                borders: data[randomnumber].borders ? data[randomnumber].borders.join(", ")     : "N/A",
-            });
+            const sotrteddata= data.sort((a,b) => a.name.common.localeCompare(b.name.common));
+            setCountry(
+                sotrteddata
+            )
+            console.log(country);
         
         } catch (err) {
             setError(err.message);
@@ -34,19 +30,44 @@ function GetCountry() {
 
     if (loading) return <div>Yuklanmoqda...</div>;
     if (error) return <div>Xato: {error}</div>;
-
-
+    
+    function setCountryContinents(continent) {
+        const filteredCountries = country.filter(item => item.continents && item.continents[0] === continent);
+        setCountry(filteredCountries);
+        continent ==''
+    }
+    function setCountryNmae(name) {
+        const filteredCountries = country.filter(item => item.name.common === name);
+        setCountry(filteredCountries);
+    }
+    function setCountryBorders(border) {
+        const filteredCountries = country.filter(item => item.borders && item.borders.includes(border));
+        setCountry(filteredCountries);
+    }
     return (
         <div id="item">
-            {country && (
-                <div key={country.name}>
-                    <h1>{country.name}</h1>
-                    <img src={country.flag} alt={country.name} style={{ width: "200px", border: "1px solid #ccc" }} />
-                    <h3>Qita: {country.qita}</h3>
-                    <p>Poytaxt: {country.poytaxt}</p>
-                    <p>Borders: {country.borders}</p>
-                </div>
-            )}
+            <form action="">
+                <select name="country" id="" onChange={(e) => setCountryContinents(e.target.value)}>
+                   
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="North America">North America</option>
+                    <option value="South America">South America</option>
+                    <option value="Africa">Africa</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
+            </form>
+            {country.map(item =>{
+                return(
+                    <div key={item.name.common} className="card" onClick={() =>  setCountryBorders(item.borders ? item.borders[0] : "")}>
+                        <img src={item.flags.svg} alt={item.name.common} className="flag" />
+                        <h3>nomi {item.name.common}</h3>
+                        <p><strong>Poytaxt:</strong> {item.capital ? item.capital[0] : "N/A"}</p>
+                        <p><strong>Qit'a:</strong> {item.continents ? item.continents[0] : "N/A"}</p>
+                        <p><strong>Chegaralar:</strong> {item.borders ? item.borders.join(", ") : "N/A"}</p>
+                    </div>
+                )
+            } ) }
         </div>
     )
 }
